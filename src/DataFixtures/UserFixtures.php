abf5117863b642c3e58d8d9ade2d643d4bc3e7b8
AhmedDWIIM <ahmedokc@gmail.com>
2021-12-15 16:47:48 +0100

    BOOTSTRAP

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    /** @var @var UserPasswordHasherInterface */
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+    public function load(ObjectManager $manager): void
+    {
+
+            $user = new User();
+            $user -> setEmail('Tadmin@admin.fr');
+            $user -> setRoles(['ROLE_ADMIN']);
+            $user -> setPassword($this->userPasswordHasher->hashPassword($user,'password'));
+            $manager -> persist($user);
+
+        $manager->flush();
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+
+    }
+}
